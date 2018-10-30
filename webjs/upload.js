57e46bbb4ceb251ef48dc21df0fd6e310314acbc
Ritsuki Goto <develop@mizucoffee.net>
2018-10-30 23:28:00 +0900

    [add] modal

--- webjs/upload.js
+++ webjs/upload.js
@@ -13,6 +13,14 @@ const sus_playlevel = document.getElementById('sus_playlevel')
 
 const sus_file = document.getElementById("sus_file")
 
+const submit_btn = document.getElementById('submit_btn')
+const submit2_btn = document.getElementById('submit2_btn')
+
+const yt_link = document.getElementById('yt_link')
+const note = document.getElementById('note')
+const check1 = document.getElementById('check1')
+const check2 = document.getElementById('check2')
+
 sus_area.onkeyup = check
 sus_area.onchange = check
 
@@ -29,21 +37,25 @@ function check() {
   sus_difficulty.value = meta.DIFFICULTY.LEVEL || ''
   sus_playlevel.value = meta.PLAYLEVEL || ''
 
-  form_stat(sus_songname,meta.TITLE)
-  form_stat(sus_songid,meta.SONGID)
-  form_stat(sus_artist,meta.ARTIST)
-  form_stat(sus_designer,meta.DESIGNER)
-  form_stat(sus_difficulty,meta.DIFFICULTY.LEVEL)
-  form_stat(sus_playlevel,meta.PLAYLEVEL)
+  let s = true
+  s = form_stat(sus_songname,meta.TITLE) ? s : false
+  s = form_stat(sus_songid,meta.SONGID) ? s : false
+  s = form_stat(sus_artist,meta.ARTIST) ? s : false
+  s = form_stat(sus_designer,meta.DESIGNER) ? s : false
+  s = form_stat(sus_difficulty,meta.DIFFICULTY.LEVEL) ? s : false
+  s = form_stat(sus_playlevel,meta.PLAYLEVEL) ? s : false
+  submit_btn.disabled = !s
 }
 
 function form_stat(target,stat) {
   if(stat) {
     target.classList.add('uk-form-success')
     target.classList.remove('uk-form-danger')
+    return true
   } else {
     target.classList.remove('uk-form-success')
     target.classList.add('uk-form-danger')
+    return false
   }
 }
 
@@ -63,3 +75,31 @@ sus_file.addEventListener('change',e => {
     reader.onload = evt => { sus_area.value = evt.target.result;sus_file.value = ""; check() }
   }
 })
+
+check1.onchange = checkbox
+check2.onchange = checkbox
+function checkbox() {
+  submit2_btn.disabled = !(check1.checked && check2.checked)
+}
+
+submit2_btn.onclick = () => {
+  const form = document.createElement('form')
+  const input = document.createElement('input')
+
+  form.method = 'POST'
+  form.action = '/score/new'
+
+  input.name = 'sus'
+  input.value = sus_area.value
+  form.appendChild(input)
+  input.name = 'yt'
+  input.value = yt_link.value
+  form.appendChild(input)
+  input.name = 'sus'
+  input.value = note.value
+  form.appendChild(input)
+
+  document.body.appendChild(form)
+  form.submit()
+  document.body.removeChild(form)
+}
